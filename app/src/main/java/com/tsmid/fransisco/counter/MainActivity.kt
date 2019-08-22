4cd559d8d068ed3ea9cd97c5a8eb64d681ba9bc7
fransisco <siscofran999@gmail.com>
2019-08-22 13:01:35 +0700

    change use swipe-button

--- app/src/main/java/com/tsmid/fransisco/counter/MainActivity.kt
+++ app/src/main/java/com/tsmid/fransisco/counter/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.tsmid.fransisco.counter
 
+import android.annotation.SuppressLint
 import android.arch.persistence.room.Room
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -23,31 +25,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         db = Room.databaseBuilder(this, AppDatabase::class.java, "orderdb").allowMainThreadQueries().build()
-        val calendar = Calendar.getInstance()
-        val mdformat = SimpleDateFormat("HH:mm")
-        val times = "Started at " + mdformat.format(calendar.time)
-        time.text = times
 
         counter2 = db?.orderDAO()?.selectCounter()!!
         Log.i(TAG, "db -> $counter2")
         counter.text = "$counter2"
 
-        switch1.setOnCheckedChangeListener { buttonView, isChecked ->
-            if(isChecked){
-                counter2 = counter2.plus(1)
-                b.counterId = counter2
-                Log.i(TAG, "true -> $counter2")
-                db?.orderDAO()?.insertCounter(b)
-                Log.i(TAG, "db2 -> ${db?.orderDAO()?.selectCounter()!!}")
-                counter.text = "$counter2"
-            }else{
-                counter2 = counter2.plus(1)
-                b.counterId = counter2
-                Log.i(TAG, "false -> $counter2")
-                db?.orderDAO()?.insertCounter(b)
-                Log.i(TAG, "db2 -> ${db?.orderDAO()?.selectCounter()!!}")
-                counter.text = "$counter2"
-            }
+        switch2.onSwipedOnListener = {
+            counter2 = counter2.plus(1)
+            b.counterId = counter2
+            time.text = setTime()
+            time.visibility = View.VISIBLE
+            db?.orderDAO()?.insertCounter(b)
+            Log.d(TAG, "onSwipedOn")
+            counter.text = "$counter2"
         }
+
+        switch2.onSwipedOffListener = {
+            counter2 = counter2.plus(1)
+            b.counterId = counter2
+            time.text = setTime()
+            db?.orderDAO()?.insertCounter(b)
+            time.text = setTime()
+            Log.d(TAG, "onSwipedOff")
+            counter.text = "$counter2"
+        }
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun setTime() : String{
+        val calendar = Calendar.getInstance()
+        val mdformat = SimpleDateFormat("HH:mm")
+        return "Started at " + mdformat.format(calendar.time)
     }
 }
